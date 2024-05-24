f92c4418c962e8954b7771b404ac4477fbe22e1a
matansegal <msegal@smu.edu>
2024-05-24 15:07:51 -0400

    get sheet

--- spreadsheets_app/utils/get_sheet.py
+++ spreadsheets_app/utils/get_sheet.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Table
+from flask import jsonify
+from sqlalchemy import Table, select
 from sqlalchemy.exc import SQLAlchemyError
 from spreadsheets_app import DATABASE, METADATA
 
@@ -11,3 +12,18 @@ def get_sheet_by_id(sheet_id):
         table = Table(table_name, METADATA, autoload_with=DATABASE.engine)
     except SQLAlchemyError as err:
         return f"could not find sheet number {sheet_id}; {err}", 400
+
+    with DATABASE.engine.connect() as conn:
+        # get the whoel sheet order by row number
+        select_statement = select(table).order_by("row_number")
+        result = conn.execute(select_statement).fetchall()
+
+        # structure sheet in dict
+        sheet = {
+            table.name: [
+                {col: val for col, val in zip(table.columns.keys(), row)}
+                for row in result
+            ]
+        }
+
+    return jsonify(sheet), 201
