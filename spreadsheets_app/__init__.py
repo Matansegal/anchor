8b521a7b7cb4b2a418e8e390a63f3532f613a67e
matansegal <msegal@smu.edu>
2024-05-23 23:24:11 -0400

    add config

--- spreadsheets_app/__init__.py
+++ spreadsheets_app/__init__.py
@@ -4,7 +4,5 @@ from sqlalchemy import MetaData
 
 
 APP = Flask(__name__)
-DATABASE_URI = "sqlite:///sheets.sqlite"
-APP.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-DATABASE = SQLAlchemy(APP)
+DATABASE = SQLAlchemy()
 METADATA = MetaData()
--- spreadsheets_app/app.py
+++ spreadsheets_app/app.py
@@ -3,10 +3,17 @@ from spreadsheets_app import APP, DATABASE
 # import those to create the routes and the models
 from spreadsheets_app import routes, models
 
-with APP.app_context():
-    DATABASE.drop_all()
-    DATABASE.create_all()
+
+def main():
+    APP.config.from_object('config.DevConfig')
+    DATABASE.init_app(APP)
+    with APP.app_context():
+        DATABASE.drop_all()
+        DATABASE.create_all()
+        
+    APP.run(host="0.0.0.0", port=int("5000"), debug=True)
+    
 
 
 if __name__ == "__main__":
-    APP.run(host="0.0.0.0", port=int("5000"), debug=True)
+    main()
